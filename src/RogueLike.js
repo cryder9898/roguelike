aca803bc027cd3dd8c6221ef7917eefe7eef7a72
Christopher Ryder <cryder9898@gmail.com>
2017-06-29 09:42:55 -0400

    added keyboard handler

--- src/RogueLike.js
+++ src/RogueLike.js
@@ -7,7 +7,44 @@ class RogueLike extends Component {
     super(props);
 
     this.state = {
-      board: []
+      gameMap: []
+    }
+    this.player = {x: 4, y: 5};
+  }
+
+  updateGameMap = (gameMap) => {
+    this.setState({gameMap: gameMap});
+  }
+
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown = (event) => {
+    event.stopPropagation();
+    let player = this.player;
+    switch(event.key) {
+      case 'ArrowUp':
+      case 'w':
+        this.movePlayer(player.x - 1, player.y);
+        break;
+      case 'ArrowDown':
+      case 's':
+        this.movePlayer(player.x + 1, player.y);
+        break;
+      case 'ArrowLeft':
+      case 'a':
+        this.movePlayer(player.x, player.y - 1);
+        break;
+      case 'ArrowRight':
+      case 'd':
+        this.movePlayer(player.x, player.y + 1);
+        break;
+      default:
     }
   }
 
