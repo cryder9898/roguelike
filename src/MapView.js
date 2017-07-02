47c990b25eb13ff1971fefcbc82d47b1d5a54367
Christopher Ryder <cryder9898@gmail.com>
2017-07-01 23:27:38 -0400

    using 2 dimensional array to go along with the map generator

--- src/MapView.js
+++ src/MapView.js
@@ -3,19 +3,34 @@ import React from 'react';
 const MapView = ({gameMap, rows, cols, tSize}) => {
 
   const style = {
-    'width': tSize*cols,
+    'margin': '0 auto',
     'lineHeight': 0
   };
 
+  const renderTile = (tile) => {
+    switch(tile) {
+      case 0: return 'wall';
+      case 1: return 'floor';
+      case 'player': return 'player';
+      default: return 'floor';
+    }
+  }
+
   return (
     <div style={style}>
-      {gameMap.map((tile, i) => {
+      {gameMap.map((row, i) => {
         return (
-          <div
-            key={i}
-            className={tile === 'player' ? 'player' : 'floor'}
-          />
-        )
+          <div key={i}>
+            {row.map((tile, j) => {
+              return (
+                <div
+                  key={[i,j]}
+                  className={renderTile(tile)}
+                />
+              );
+            })}
+          </div>
+        );
       })}
     </div>
   );
